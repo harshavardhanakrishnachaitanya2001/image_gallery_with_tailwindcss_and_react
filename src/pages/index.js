a2d146b35e4f93bca4b304979fb0988cb758d147
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2023-03-28 00:37:59 +0530

    Trying to do something

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,8 @@ export async function getStaticProps(){
   return {
     props:{
       images:data,
-    }
+    },
+    revalidate: 60
   }
 }
 
@@ -39,7 +40,7 @@ export default function Gallery({images}){
 function BlurImage({image}){
   const [isLoading,setLoading] = useState(true)
   return (
-    <a href="{image.href}" className="group">
+    <a href={image.href} className="group">
     <div className="w-full aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 overflow-hidden rounded-lg bg-gray-200">
       <Image alt="" 
       src={image.imageSrc} 
