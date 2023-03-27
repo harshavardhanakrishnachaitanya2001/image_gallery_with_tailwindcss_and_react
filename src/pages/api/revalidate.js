c813ff593c842b92999141a6b9600e1d36eee5eb
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2023-03-27 23:28:58 +0530

    updated on-demand ISR

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,7 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req,res){
-  if(req.query!==process.env.REVALIDATE_SECRET){
+  if(req.query.secret!==process.env.REVALIDATE_SECRET){
     return res.status(401).json({message:'Invalid token'})
   }
   try{
