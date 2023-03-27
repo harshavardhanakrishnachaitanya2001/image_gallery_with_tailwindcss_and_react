d98dbc96f0f38d87ce4cfd01b3083717775d444e
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2023-03-27 23:40:38 +0530

    Think I made dynamic

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,8 +50,8 @@ function BlurImage({image}){
       :'grayscale-0 blur-0 scale-100')}
       onLoadingComplete = {() => setLoading(false)}/>
     </div>
-    <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
-    <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
+    <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+    <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
   </a>
   )
   
